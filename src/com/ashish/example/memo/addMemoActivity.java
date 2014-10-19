be24735e6f38bfce6d2027bda94702ce91cb2e8a
Ashish Singh <imrealashu@gmail.com>
2014-10-19 23:47:43 +0530

    Application Initiated

---
+++ src/com/ashish/example/memo/addMemoActivity.java
@@ -0,0 +1,15 @@
+package com.ashish.example.memo;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class addMemoActivity extends ActionBarActivity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.add_memo);
+	}
+	
+}
